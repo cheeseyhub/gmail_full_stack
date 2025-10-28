b66770935691a0939d3a0024671fee5c36f4b233
CheeseyHub <muhammedibrahim613@gmail.com>
2025-10-28 18:26:28 +0500

    connected to db

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,30 @@
 import express from "express";
 import GetRouter from "./routes/GetRouter.js";
 import SendRouter from "./routes/SendRouter.js";
-
+import mongoose from "mongoose";
+import { loadEnvFile } from "node:process";
+loadEnvFile();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json())
+app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/", (req, res, next) => {
-  res.send("This is the main page")
-
-})
+  res.send("This is the main page");
+});
 app.use("/get", GetRouter);
 app.use("/send", SendRouter);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log("Error connecting to the database.");
+  });
 
 app.listen(port, () => {
   console.log(`Server is listening on the port  ${port}`);
